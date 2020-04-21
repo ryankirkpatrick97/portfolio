363a4c17a28348f1ce269664c89aa3e72bd20b7f
Ryan Kirkpatrick <ryankirkpatrick97@gmail.com>
2020-04-20 22:09:24 -0700

    Made it so that images in images.html will adapt to screen changing size

--- scripts.js
+++ scripts.js
@@ -15,3 +15,23 @@ function showLightbox(t){
     lBox.style.display = "inline-block";
     overlayImg.src = t.src;
 }
+
+function resizeImagesPage(x) {
+    // Get the elements with class="column"
+    var elements = document.getElementsByClassName("column");
+
+    // Declare a loop variable
+    var i;
+    if (x.matches){
+        for (i = 0; i < elements.length; i++) {
+            elements[i].style.msFlex = "100%";  // IE10
+            elements[i].style.flex = "100%";
+        }    
+    }
+    else{
+        for (i = 0; i < elements.length; i++) {
+            elements[i].style.msFlex = "33%";  // IE10
+            elements[i].style.flex = "33%";
+        }
+    }
+}
