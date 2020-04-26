787e13e152ad3d0da56d5174470324bb76c5c9f8
Ryan Kirkpatrick <ryankirkpatrick97@gmail.com>
2020-04-25 20:53:48 -0700

    Got videos page running again

--- portfolio/src/Components/Videos.js
+++ portfolio/src/Components/Videos.js
@@ -3,9 +3,24 @@ import React, {Component} from 'react'
 export class Videos extends Component{
     render(){
         return (
-            <div>
-                <h2>Videos</h2>
-            </div> 
+        <div>
+            <div><h1>Capstone Demo: 2018-2019</h1></div>
+            <div className="container">
+                <iframe class="video" src="https://www.youtube.com/embed/XMk__Djtibg?wmode=opaque" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div><h1>ECE 153 Project</h1></div>
+            <div className="container">
+                <iframe class="video" src="https://www.youtube.com/embed/9jjcRcZ2lSg?wmode=opaque" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div><h1>ECE179D Robotics Project 1</h1></div>
+            <div className="container">
+                <iframe class="video" src="https://www.youtube.com/embed/fQDsw4yPw_c?wmode=opaque" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div><h1>ECE179D Robotics Project 2</h1></div>
+            <div className="container">
+                <iframe class="video" src="https://www.youtube.com/embed/v-9yMAVkSok?wmode=opaque" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
         )
     };
 }
